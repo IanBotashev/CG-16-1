4a0e59ee424b87daf4613b759274e1df18cc2b2e
IanBotashev <yanbotashev@gmail.com>
2024-07-14 02:42:41 -0700

    New exception tests

--- helpers/assembler/tests/test_tokenizer.py
+++ helpers/assembler/tests/test_tokenizer.py
@@ -1,6 +1,7 @@
 import pytest
 from helpers.assembler.tokenizer.tokenizer import Tokenizer
 from helpers.assembler.assembler_types import Instruction, Argument
+from helpers.assembler.exceptions import InvalidSyntax
 
 
 @pytest.fixture
@@ -24,8 +25,6 @@ def test_tokenizer_fixture(tokenizer):
         ("AdC", Instruction("adc")),
         ("int", Instruction("int")),
         ("MOv", Instruction("mov")),
-        pytest.param("NOT_AN_INSTRUCTION", None, marks=pytest.mark.xfail),
-        pytest.param("3", None, marks=pytest.mark.xfail),
     ])
 def test_instruction_tokenizing(tokenizer, string_to_parse, expected_token):
     """
@@ -38,15 +37,29 @@ def test_instruction_tokenizing(tokenizer, string_to_parse, expected_token):
 
 
 @pytest.mark.parametrize(
+    "string_to_parse, expected_exception",
+    [
+        ("NOT_AN_INSTRUCTION", InvalidSyntax),
+        ("3", InvalidSyntax),
+    ])
+def test_instruction_tokenizing_with_illegal_syntax(tokenizer, string_to_parse, expected_exception):
+    """
+    Tests if the tokenizer raises the appropriate exception when attempting to parse invalid strings.
+    :param tokenizer:
+    :param string_to_parse:
+    :param expected_exception:
+    :return:
+    """
+    with pytest.raises(expected_exception):
+        tokens = tokenizer.parse(string_to_parse)
+
+
+@pytest.mark.parametrize(
     "string_to_parse, expected_token",
     [
         ("MOV 1", Instruction("mov", arguments=[Argument(1, address=False)])),
         ("aDC $2", Instruction("adc", arguments=[Argument(2, address=True)])),
         ("int $3, 4,$5, $6 ,      7", Instruction("int", arguments=[Argument(3, address=True), Argument(4, address=False), Argument(5, address=True), Argument(6, address=True), Argument(7, address=False)])),
-        pytest.param("MoV invalid", None, marks=pytest.mark.xfail),
-        pytest.param("MoV 1 2 $3", None, marks=pytest.mark.xfail),
-        pytest.param("MoV $1 $2", None, marks=pytest.mark.xfail),
-        pytest.param("MoV $1 2 3", None, marks=pytest.mark.xfail),
     ])
 def test_argument_tokenizing(tokenizer, string_to_parse, expected_token):
     """
@@ -56,3 +69,23 @@ def test_argument_tokenizing(tokenizer, string_to_parse, expected_token):
     tokens = tokenizer.parse(string_to_parse)
     assert isinstance(tokens, list)
     assert tokens[0] == expected_token
+
+
+@pytest.mark.parametrize(
+    "string_to_parse, expected_exception",
+    [
+        ("MoV invalid", InvalidSyntax),
+        ("MoV 1 2 $3", InvalidSyntax),
+        ("MoV $1 $2", InvalidSyntax),
+        ("MoV $1 2 3", InvalidSyntax),
+    ])
+def test_argument_tokenizing_with_illegal_syntax(tokenizer, string_to_parse, expected_exception):
+    """
+    Tests if these cases end up as expected exceptions
+    :param tokenizer:
+    :param string_to_parse:
+    :param expected_exception:
+    :return:
+    """
+    with pytest.raises(expected_exception):
+        tokenizer.parse(string_to_parse)
