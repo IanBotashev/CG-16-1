49c162b2fd6c53291fb790425ad828a9c1b2d84f
IanBotashev <yanbotashev@gmail.com>
2024-07-14 18:01:33 -0700

    moved assembler to https://github.com/IanBotashev/CG-16-1-Assembler

--- helpers/assembler/exceptions.py
+++
@@ -1,2 +0,0 @@
-class InvalidSyntax(Exception):
-    pass
--- helpers/assembler/tests/test_general.py
+++
@@ -1,6 +0,0 @@
-def test_check():
-    """
-    Just to make sure. If this is false, something has gone horribly wrong.
-    :return:
-    """
-    assert 1 + 1 == 2
